2cf32b6d0bfb66140b80cd6315482ddef45ae46e
jfmisasm <jfmisasm@unal.edu.co>
2016-09-29 17:28:35 -0500

    commit

--- benchmark/src/benchmark/Double.java
+++ benchmark/src/benchmark/Double.java
@@ -1,7 +1,5 @@
 package benchmark;
 
-import java.util.ArrayList;
-
 public class Double {
 	
 	private double arrayTest[] = new double[1000000];
@@ -79,28 +77,5 @@ public class Double {
 		}
 		time.stop(); return time.tomo();
 	}
-	
-	private void quickSort(ArrayList<Double> arrayDouble, Double left, Double right) {
-		Double X = arrayDouble.get(left); // tomamos primer elemento como pivote
-		  int i=left; // i realiza la búsqueda de izquierda a derecha
-		  int j=right; // j realiza la búsqueda de derecha a izquierda
-		  int aux;
-		 
-		  while(i<j){            // mientras no se crucen las búsquedas
-		     while(A[i]<=X && i<j) i++; // busca elemento mayor que pivote
-		     while(A[j]>X) j--;         // busca elemento menor que pivote
-		     if (i<j) {                      // si no se han cruzado                      
-		         aux= A[i];                  // los intercambia
-		         A[i]=A[j];
-		         A[j]=aux;
-		     }
-		   }
-		   A[left]=A[j]; // se coloca el pivote en su lugar de forma que tendremos
-		   A[j]=X; // los menores a su izquierda y los mayores a su derecha
-		   if(left<j-1)
-		      quickSort(A,left,j-1); // ordenamos subarray izquierdo
-		   if(j+1 <right)
-		      quickSort(A,j+1,right); // ordenamos subarray derecho
-		}
 	
 }
