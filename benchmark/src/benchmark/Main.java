62fbe02b84276476b8e100a283e2453fcae03351
jfmisasm <jfmisasm@unal.edu.co>
2016-10-14 23:35:05 -0500

    :v

--- benchmark/src/benchmark/Main.java
+++ benchmark/src/benchmark/Main.java
@@ -4,12 +4,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import benchmark.tiempo.*;
 
 import benchmark.tiempo;
 public class Main {
 	
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
 		Double operacionesDouble = new Double();
 		Entero operacionesEntero = new Entero();
 		int ArregloEnteros[]=new int [1000000];
@@ -24,6 +27,19 @@ public class Main {
 		ArregloOrdenadoEnteros=cargar.leerQuickEnteros();
 		ArregloOrdenadoDoubles=cargar.leerDoubles();
 		
+		int m;
+		int i = 1;
+		while ( i == 1 ) {
+			System.out.println("1. Correr el Becnchmark.");
+			System.out.println("2. Salir.");
+			m = entrada.next();  :v ni idea que no me acuerdo del input :v
+			if ( m == 1 ) {
+				creadorArreglos misArreglos = new creadorArreglos();
+			}else if ( m == 2 ) {
+				i = 0;
+				System.out.println("bye!");
+			}
+		}
 		
 		
 	}
