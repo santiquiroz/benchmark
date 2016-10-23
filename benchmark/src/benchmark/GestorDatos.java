16768f5f11aeea6081bb7cfe6b92d5248d455bdc
jfmisasm <jfmisasm@unal.edu.co>
2016-10-23 17:55:21 -0500

    pirateo

--- benchmark/src/benchmark/GestorDatos.java
+++ benchmark/src/benchmark/GestorDatos.java
@@ -3,13 +3,15 @@ package benchmark;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-//CLASE MACHETAZO A LA "lectura" ORIGINAL
-public class Lektura {
+//Código robado de SuPropiedadRaiz, copyright by Santiago, pirateado by Juancho  :v
+public class GestorDatos {
 	
-	private static File archivo = new File("datos");
+private static File archivo = new File("datos");
 	
 	public static Datos leerDatos() {
 		Datos datos = null;
@@ -38,6 +40,29 @@ public class Lektura {
 			}
 		}
 		return datos;
+	}
+	
+	public static void guardarDatos(Datos data) {
+		FileOutputStream fw = null;
+		ObjectOutputStream ow = null;
+		try {
+			fw = new FileOutputStream(archivo);
+			ow = new ObjectOutputStream(fw);
+			ow.writeObject(data);
+		} catch(IOException e) {
+			System.out.println("Error: " + e.toString());
+		} finally {
+			try {
+				if (ow != null) {
+					ow.close();
+				}
+				if (fw != null) {
+					fw.close();
+				}
+			} catch(IOException e) {
+				System.out.println("Error: " + e.toString());
+			}
+		}
 	}
 	
 }
